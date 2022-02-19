8f24d104c8f0c494ad7a35da4c213813f92220d1
Farid ullah <faridullah996@gmail.com>
2022-02-19 15:07:19 +0500

    helmet package for http headers (security)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -14,14 +15,20 @@ const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
+// MIDDLEWARES
+// 1) Set security http headers;
+app.use(helmet());
+
+// 2) Development logging;
 if (process.env.NODE_ENV === 'development ') {
 	app.use(morgan('dev'));
 }
 
-// Setup pug engine
+// 3) Setup pug engine
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
+// 4) Limit request from same IP;
 const limiter = rateLimit({
 	windowMs: 60 * 60 * 1000, // 15 minutes
 	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
@@ -31,7 +38,10 @@ const limiter = rateLimit({
 // Apply the rate limiting middleware to all requests
 app.use('/api', limiter);
 
-app.use(express.json());
+// 5) Body parser; reading data from request body;
+app.use(express.json({ limit: '10kb'}));
+
+// 6) Serving static files;
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
