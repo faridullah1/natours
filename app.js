170d77ebe4c512de7b23d1ca5fc883f57d2578ec
Farid ullah <faridullah996@gmail.com>
2022-02-19 14:57:43 +0500

    rate limiting using express rate limit package.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const path = require('path');
+
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -19,6 +21,15 @@ if (process.env.NODE_ENV === 'development ') {
 // Setup pug engine
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+
+const limiter = rateLimit({
+	windowMs: 60 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	message: 'To many requests from this IP, please try again after 1 hour.'
+});
+
+// Apply the rate limiting middleware to all requests
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
