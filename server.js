ad672d00517d73ddfb12a65040259bfe3d54af14
Farid ullah <faridullah996@gmail.com>
2022-02-24 11:45:42 +0500

    minor fix.

--- server.js
+++ server.js
@@ -14,15 +14,18 @@ const app = require('./app');
 
 mongoose.connect(process.env.DATABASE).then(() => {
 	console.log('DB connection successfull.');
-});
-
-// const LOCAL_DB = process.env.DATABASE_LOCAL;
-mongoose.connect(LOCAL_DB).then(() => {
-	console.log('DB connection successfull.');
 }).catch(err => {
 	console.log(err.name, err.message);
 	process.exit(1);
-});
+});;
+
+// const LOCAL_DB = process.env.DATABASE_LOCAL;
+// mongoose.connect(LOCAL_DB).then(() => {
+// 	console.log('DB connection successfull.');
+// }).catch(err => {
+// 	console.log(err.name, err.message);
+// 	process.exit(1);
+// });
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
